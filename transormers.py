71043a756a6977954989ffe2d2ef9bddba9576d0
Jason Robinson <robinsonjason761@gmail.com>
2022-06-11 02:42:04 -0500

    Updating transformer file

---
+++ transormers.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod, ABC
+
+
+class Transformation(ABC):
+    """
+    A class used to represent a Trasnformation Type
+    ...
+    Attributes
+    ----------
+    name : str
+        the name of the transform to show
+    suffix : str
+        suffix of the transform to show
+    units_show : str
+        units to show when transform is applied
+    Methods
+    -------
+    transform(search_word)
+        Do the transformation
+    """
+    def __init__(self, **kwargs):
+        assert kwargs.get('name', None), "Must define a name"
+        assert kwargs.get('suffix', None), "Must define a suffix"
+
+        self.name = kwargs.get('name')
+        self.suffix = "_{}".format(kwargs.get('suffix'))
+        self.units_show = kwargs.get('units_show', None)
+
+    @abstractmethod
+    def transform(self):
+        pass
